118d4ee9dff7dbed79b9033a20e104452ce180a0
mk-jeon <genspark_dev@genspark.ai>
2026-01-29 02:09:44 +0000

    fix: Cloudflare Pages 배포를 위한 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
+import build from '@hono/vite-build/cloudflare-pages'
+import devServer from '@hono/vite-dev-server/cloudflare'
 
 export default defineConfig({
   plugins: [
-    build(),
+    build({
+      entry: 'src/index.tsx'
+    }),
     devServer({
-      adapter,
       entry: 'src/index.tsx'
     })
   ]
